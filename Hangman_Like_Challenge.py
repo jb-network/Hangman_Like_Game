5850ba967124e750063ddcb5086cd0ea836bbf4a
jb-network <85036487+jb-network@users.noreply.github.com>
2022-10-06 21:56:54 -0500

    Update Hangman_Like_Challenge.py

--- Hangman_Like_Challenge.py
+++ Hangman_Like_Challenge.py
@@ -1,8 +1,11 @@
 from random import choice
+import string
 
 
 def start_game():
     # Var List
+    start_user_health = 6
+    start_round_tracker = 1
     user_difficulty = 0
     difficulty_map = {1: "easy", 2: "average", 3: "insane"}
     easy_list = ["cat", "dog", "egg", "beef", "hand"]
@@ -16,7 +19,7 @@ def start_game():
     for skill_number, skill_level in difficulty_map.items():
         print(f"{skill_number}: {skill_level}")
     while user_difficulty not in range(1, 4):
-        user_difficulty = int(input("\nPick your difficulty: [1,2,3]"))
+        user_difficulty = int(input("\nPick your difficulty [1,2,3]: "))
         continue
     print(f"\nYou selected the: {difficulty_map[user_difficulty]} wordlist, good luck!")
 
@@ -27,10 +30,56 @@ def start_game():
         game_wordlist = choice(average_list)
     else:
         game_wordlist = choice(insane_list)
-    return game_wordlist
+    input("Press 'Enter' to start the game")
+    game_word_blank = blank_maker(game_wordlist)
+    return game_wordlist, start_user_health, start_round_tracker, game_word_blank
+
+
+def blank_maker(game_wordlist):
+    game_word_blank = ""
+    word_len_loop = 0
+    while word_len_loop < len(game_wordlist):
+        game_word_blank = game_word_blank + '-'
+        word_len_loop += 1
+    return game_word_blank
+
+
+def game_menu(game_word_blank, user_health, round_tracker):
+    valid_letters = approved_letters()
+    valid_guess = False
+    print("\n***********************************************")
+    print(f"ROUND:{round_tracker}")
+    print(f"Player Health:{user_health}")
+    print(f"Word:{game_word_blank}")
+    # NEED TO ADD USED LETTERS
+    print("************************************************")
+    while not valid_guess:
+        user_letter_choice = input("Guess a letter:")
+        for letter in valid_letters:
+            if letter == user_letter_choice:
+                valid_guess = True
+                print(f"You selected the letter: {letter}")
+                break
+            else:
+                continue
+    return valid_letters
+
+
+def approved_letters():
+    lower_letters = string.ascii_lowercase
+    valid_letters = []
+    for letter in lower_letters:
+        valid_letters.append(letter)
+    return valid_letters
 
 
 # Test input
-print(start_game())
+game_over = False
+winning_word, user_health, round_tracker, game_word_blank = (start_game())
 
-# More to follow, this was just the first function.
+while not game_over:
+    valid_letter = game_menu(game_word_blank, user_health, round_tracker)
+    game_over = True
+    # Make method that will compare letter, adjust blank, player health
+
+# More to follow, this is an ongoing work in progress.
