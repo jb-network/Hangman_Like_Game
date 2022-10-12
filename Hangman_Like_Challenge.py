8b47188929e68852dc993b4cac31185fb75069ba
jb-network <85036487+jb-network@users.noreply.github.com>
2022-10-11 22:47:28 -0500

    Update Hangman_Like_Challenge.py

--- Hangman_Like_Challenge.py
+++ Hangman_Like_Challenge.py
@@ -2,7 +2,6 @@ from random import choice
 import string
 
 def start_game():
-    # Var List
     start_user_health = 6
     start_round_tracker = 1
     user_difficulty = 0
@@ -12,7 +11,10 @@ def start_game():
     average_list = ["goodbye", "letter", "ground", "snow", "water"]
     insane_list = ["gizmo", "peekaboo", "zigzagging", "fluffiness", "pneumonia"]
     game_word_blank = []
-    # User menu, use difficulty_map to unpack choices
+    fully_matched = False
+    game_over = False
+    remove_health_tracker = True
+
     print("Welcome to Guess or Die")
     print("I will pick a word and you have 6 incorrect guesses to get it right or you die")
     print("\nGame difficulty:")
@@ -22,8 +24,6 @@ def start_game():
         user_difficulty = int(input("\nPick your difficulty [1,2,3]: "))
         continue
     print(f"\nYou selected the: {difficulty_map[user_difficulty]} wordlist, good luck!")
-
-    # Pick a word based using random.choice and user selection
     if user_difficulty == 1:
         game_wordlist = list(choice(easy_list))
     elif user_difficulty == 2:
@@ -33,8 +33,7 @@ def start_game():
     input("Press 'Enter' to start the game")
     game_word_blank = blank_maker(game_wordlist, game_word_blank)
     valid_letters = approved_letters()
-    return game_wordlist, start_user_health, start_round_tracker, game_word_blank, valid_letters, guessed_letters
-
+    return game_wordlist, start_user_health, start_round_tracker, game_word_blank, valid_letters, guessed_letters, fully_matched, game_over, remove_health_tracker
 
 def blank_maker(game_wordlist, game_word_blank):
     game_word_blank = game_wordlist.copy()
@@ -42,14 +41,12 @@ def blank_maker(game_wordlist, game_word_blank):
         game_word_blank[index] = "_"
     return game_word_blank
 
-
 def approved_letters():
     lower_letters = string.ascii_lowercase
     valid_letters = []
     for letter in lower_letters:
         valid_letters.append(letter)
     return valid_letters
-
 
 def game_menu(game_word_blank, user_health, round_tracker, valid_letters, guessed_letters):
     valid_guess = False
@@ -74,7 +71,6 @@ def game_menu(game_word_blank, user_health, round_tracker, valid_letters, guesse
                 continue
     return valid_letters,letter_tracker_update, user_letter_choice, game_word_blank
 
-
 def guessed_letter_sorter(guessed_letters):
     fixed_guess_string = ",".join(sorted(guessed_letters))
     return fixed_guess_string
@@ -86,36 +82,51 @@ def  update_used_letters(guessed_letters, user_letter_choice):
         guessed_letters.append(user_letter_choice)
     return guessed_letters
 
-def game_updates(round_tracker, letter_tracker_update, valid_letter, user_letter_choice,game_word_blank,game_wordlist):
+def game_updates(round_tracker, letter_tracker_update, valid_letter, user_letter_choice,game_word_blank,game_wordlist, fully_matched, game_over, user_health, remove_health_tracker):
     round_tracker += 1
+    remove_health_tracker = True
     guessed_letters == letter_tracker_update
     valid_letter = valid_letter_update(valid_letter, user_letter_choice)
-    game_word_blank = word_blank_update(game_wordlist, game_word_blank, user_letter_choice)
-    return round_tracker, guessed_letters, valid_letter, game_word_blank
+    game_word_blank, fully_matched, remove_health_tracker = word_blank_update(game_wordlist, game_word_blank, user_letter_choice, fully_matched, remove_health_tracker)
+    if remove_health_tracker == True:
+        user_health -=1
+    if fully_matched:
+        print("\n************************************************")
+        print("YOU WIN!")
+        print("YOU GUESSED THE CORRECT WORD:")
+        blank_display = print(*game_wordlist, sep="")
+        print("************************************************")
+        game_over = True
+    if user_health == 0:
+        print("\n************************************************")
+        print("YOU DIED!")
+        print("DID NOT GUESS THE CORRECT WORD:")
+        blank_display = print(*game_wordlist, sep="")
+        print("************************************************")
+        game_over = True
+    return round_tracker, guessed_letters, valid_letter, game_word_blank, fully_matched, game_over, user_health, remove_health_tracker
 
 def valid_letter_update(valid_letters, user_letter_choice):
+
     for letter in valid_letters:
         if letter == user_letter_choice:
             valid_letters.remove(letter)
     return valid_letters
 
-
-def word_blank_update(game_wordlist, game_word_blank, user_letter_choice):
+def word_blank_update(game_wordlist, game_word_blank, user_letter_choice, fully_matched, remove_health_tracker):
         for index, item in enumerate(game_wordlist):
             if user_letter_choice == game_wordlist[index]:
                 game_word_blank[index] = user_letter_choice
-        return game_word_blank
+                remove_health_tracker = False
+        if game_word_blank == game_wordlist:
+                fully_matched = True
+        return game_word_blank, fully_matched, remove_health_tracker
 
-# Test input
-game_over = False
-game_wordlist, user_health, round_tracker, game_word_blank, valid_letters, guessed_letters= (start_game())
+
+
+game_wordlist, user_health, round_tracker, game_word_blank, valid_letters, guessed_letters, fully_matched, game_over, remove_health_tracker = (start_game())
 
 while not game_over:
-
     valid_letters, letter_tracker_update, user_letter_choice, game_word_blank = game_menu(game_word_blank, user_health, round_tracker, valid_letters, guessed_letters)
-    round_tracker, letter_tracker_update, valid_letters, game_word_blank = game_updates(round_tracker,letter_tracker_update, valid_letters, user_letter_choice, game_word_blank, game_wordlist)
-    #game_over = True
+    round_tracker, letter_tracker_update, valid_letters, game_word_blank, fully_matched, game_over, user_health, remove_health_tracker = game_updates(round_tracker,letter_tracker_update, valid_letters, user_letter_choice, game_word_blank, game_wordlist, fully_matched, game_over, user_health, remove_health_tracker)
 
-    # Make method that will compare letter, adjust blank, player health
-
-# More to follow, this is an ongoing work in progress.
